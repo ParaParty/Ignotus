528ea881eca2108b6d94729d4c6c899d67cf6e5d
Eric_Lian <git@superexercisebook.com>
2021-11-25 15:29:49 +0800

    docs: 补充文档

--- validation/src/main/java/com/tairitsu/ignotus/validation/AttributeValidatorInterface.kt
+++ validation/src/main/java/com/tairitsu/ignotus/validation/AttributeValidatorInterface.kt
@@ -1,11 +1,16 @@
 package com.tairitsu.ignotus.validation
 
+/**
+ * 验证器接口
+ */
 interface AttributeValidatorInterface : (String, Any?, Any?, (String) -> Unit) -> Unit {
     /**
-     * [attribute]: 属性名
-     * [arg]: 验证器传入的参数
-     * [value]: 被验证的值
-     * [fail]: 验证失败的回调函数，如果验证成功就别调这个函数了
+     * 验证器
+     *
+     * @param [attribute]: 属性名
+     * @param [arg]: 验证器传入的参数
+     * @param [value]: 被验证的值
+     * @param [fail]: 验证失败的回调函数，如果验证成功就别调这个函数了
      */
     override fun invoke(attribute: String, arg: Any?, value: Any?, fail: (String) -> Unit)
 }
