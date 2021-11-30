2021f4df510851b642adbb5d06424214c5f99e22
Eric_Lian <git@superexercisebook.com>
2021-11-30 16:30:48 +0800

    feat: 加点 JSON 常用功能封装

--- support/src/main/java/com/tairitsu/ignotus/support/util/JSON.kt
+++ support/src/main/java/com/tairitsu/ignotus/support/util/JSON.kt
@@ -1,5 +1,6 @@
 package com.tairitsu.ignotus.support.util
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -19,9 +20,35 @@ object JSON {
         return mapper.readValue(content, clazz)
     }
 
+    @JvmStatic
+    fun <T> parseObject(content: String, type: TypeReference<T>): T? {
+        return mapper.readValue(content, type)
+    }
+
+    @JvmStatic
+    fun parseObjectToMap(content: String): Map<String, Any> {
+        return mapper.readValue(content, object : TypeReference<Map<String, Any>>() {})
+    }
+
     fun <T> String.jsonToObject(clazz: Class<T>): T? {
         return try {
             mapper.readValue(this, clazz)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun <T> String.jsonToObject(type: TypeReference<T>): T? {
+        return try {
+            mapper.readValue(this, type)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun String.jsonToMap(): Map<String, Any>? {
+        return try {
+            mapper.readValue(this, object : TypeReference<Map<String, Any>>() {})
         } catch (e: Exception) {
             null
         }
