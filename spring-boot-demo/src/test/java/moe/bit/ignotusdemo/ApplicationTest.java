8d345eb6c2f70a90e221834a2454c9484af2135d
Eric_Lian <git@superexercisebook.com>
2021-12-06 23:01:50 +0800

    feat: 导入单测依赖
    909706381e301729e17ce36c2405d44af4f70348

---
+++ spring-boot-demo/src/test/java/moe/bit/ignotusdemo/ApplicationTest.java
@@ -0,0 +1,26 @@
+package moe.bit.ignotusdemo;
+
+import moe.bit.ignotusdemo.service.implement.JpaExampleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/12/6 10:17
+ */
+@SpringBootTest
+public class ApplicationTest {
+    @Resource
+    JpaExampleService jpaExampleService;
+
+    @Test
+    public void f() {
+        System.out.println(jpaExampleService);
+        System.out.println("EricNB");
+    }
+
+}
