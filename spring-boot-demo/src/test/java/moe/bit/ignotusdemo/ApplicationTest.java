af706be6df5d40a4cf5ac19d44b9e2c8c4c712e4
Eric_Lian <git@superexercisebook.com>
2021-12-07 20:41:27 +0800

    refactor: 将 jpa 例子单独存放

--- spring-boot-demo/src/test/java/moe/bit/ignotusdemo/ApplicationTest.java
+++ spring-boot-demo/src/test/java/moe/bit/ignotusdemo/ApplicationTest.java
@@ -1,36 +1,9 @@
 package moe.bit.ignotusdemo;
 
-import moe.bit.ignotusdemo.dao.JpaExampleDao;
-import moe.bit.ignotusdemo.model.entity.JpaExampleEntity;
-import moe.bit.ignotusdemo.service.implement.JpaExampleService;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
 
-/**
- * @author 13090
- * @version 1.0
- * @description: TODO
- * @date 2021/12/6 10:17
- */
 @SpringBootTest
 public class ApplicationTest {
-    @Resource
-    JpaExampleService jpaExampleService;
-
-    @Test
-    public void f() {
-        System.out.println(jpaExampleService);
-        System.out.println("EricNB");
-    }
-
-    @Resource
-    JpaExampleDao jpaExampleDao;
-
-    @Test
-    public void testSave() {
-        jpaExampleDao.save(new JpaExampleEntity());
-    }
 
 }
---
+++ spring-boot-jpa-demo/src/main/java/moe/bit/ignotusdemo/Application.java
@@ -0,0 +1,13 @@
+package moe.bit.ignotusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
