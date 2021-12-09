668c51f95dc9f37e857b9d076ff72ac07dc34ff5
Eric_Lian <git@superexercisebook.com>
2021-12-09 11:22:48 +0800

    feat: 添加 JsonApi 请求体泛型

---
+++ foundation/src/main/java/com/tairitsu/ignotus/foundation/model/JsonApiBody.kt
@@ -0,0 +1,31 @@
+package com.tairitsu.ignotus.foundation.model
+
+import com.tairitsu.ignotus.validation.annotation.Required
+
+/**
+ * 一个 Json Api 格式的对象
+ */
+class JsonApiObject<T> {
+    var type: String? = null
+
+    var id: String? = null
+
+    @Required
+    var attributes: T? = null
+}
+
+/**
+ * 一个由单个 Json Api 对象组成的请求体
+ */
+class JsonApiObjectBody<T> {
+    @Required
+    lateinit var data: JsonApiObject<T>
+}
+
+/**
+ * 一个由多个 Json Api 对象组成的请求体
+ */
+class JsonApiListBody<T> {
+    @Required
+    lateinit var data: List<JsonApiObject<T>>
+}
