98c4945ea0185dace039230bebbf4d2c97db1927
Eric_Lian <git@superexercisebook.com>
2022-06-01 15:19:24 +0800

    refactor: 优化导入列表

--- support/src/main/java/com/tairitsu/ignotus/support/service/DefaultJSONMapperProvider.kt
+++ support/src/main/java/com/tairitsu/ignotus/support/service/DefaultJSONMapperProvider.kt
@@ -3,10 +3,6 @@ package com.tairitsu.ignotus.support.service
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import com.tairitsu.ignotus.support.config.JacksonNamingStrategyConfig
 
 class DefaultJSONMapperProvider() : JSONMapperProvider {
